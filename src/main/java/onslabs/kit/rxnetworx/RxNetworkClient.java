cb861fc752088e1d134f19aa845a93a8076f9a38
JKumar <JKumar@TempLabs.local>
2017-08-04 14:38:18 +0530

    Enabled https adapter

--- src/main/java/onslabs/kit/rxnetworx/RxNetworkClient.java
+++ src/main/java/onslabs/kit/rxnetworx/RxNetworkClient.java
@@ -8,11 +8,21 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.CertificatePinner;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -23,6 +33,9 @@ import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RxNetworkClient {
+
+    private static SSLContext mSSLContext;
+    private static X509TrustManager mTrustManager;
 
     private RxNetworkClient() {
     }
@@ -88,6 +101,110 @@ public class RxNetworkClient {
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
     }
+
+
+    private static void createKeyStore(InputStream  certificateInputStream) {
+
+        try {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            Certificate ca;
+            try {
+                ca = cf.generateCertificate(certificateInputStream);
+                System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+            } finally {
+                certificateInputStream.close();
+            }
+
+        // Create a KeyStore containing our trusted CAs
+            String keyStoreType = KeyStore.getDefaultType();
+            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", ca);
+
+        // Create a TrustManager that trusts the CAs in our KeyStore
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(keyStore);
+            mTrustManager = (X509TrustManager) tmf.getTrustManagers()[0];
+
+        // Create an SSLContext that uses our TrustManager
+            mSSLContext = SSLContext.getInstance("TLS");
+            mSSLContext.init(null, tmf.getTrustManagers(), null);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("SOP from no IS method " + ex.getLocalizedMessage());
+        }
+    }
+
+    public static Retrofit getHttpsRestAdapter(final String hostName, final CertificatePinner certificatePinner,final InputStream inputStream, final String baseUrl, final HashMap<String, String> requestHeaderMap) {
+
+        if (mTrustManager == null || mSSLContext == null) {
+            createKeyStore(inputStream);
+        }
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS);
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        Gson gson = new GsonBuilder().addSerializationExclusionStrategy(new ExclusionStrategy() {
+            @Override
+            public boolean shouldSkipField(FieldAttributes fieldAttributes) {
+                final Expose expose = fieldAttributes.getAnnotation(Expose.class);
+                return expose != null && !expose.serialize();
+            }
+
+            @Override
+            public boolean shouldSkipClass(Class<?> aClass) {
+                return false;
+            }
+        }).addDeserializationExclusionStrategy(new ExclusionStrategy() {
+            @Override
+            public boolean shouldSkipField(FieldAttributes fieldAttributes) {
+                final Expose expose = fieldAttributes.getAnnotation(Expose.class);
+                return expose != null && !expose.deserialize();
+            }
+
+            @Override
+            public boolean shouldSkipClass(Class<?> aClass) {
+                return false;
+            }
+        }).create();
+
+        OkHttpClient client = new OkHttpClient.Builder().
+                sslSocketFactory(mSSLContext.getSocketFactory(), mTrustManager)
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request.Builder builder = chain.request().newBuilder();
+                        Set<Map.Entry<String, String>> entrySet = requestHeaderMap.entrySet();
+                        for (Map.Entry<String, String> entry : entrySet) {
+                            if (entry.getValue() != null) {
+                                if (entry.getValue().isEmpty())
+                                    builder.removeHeader(entry.getKey());
+                                else
+                                    builder.addHeader(entry.getKey(), entry.getValue());
+                            }
+                        }
+
+                        Request request = builder.build();
+                        return chain.proceed(request);
+                    }
+
+
+                })
+                .addInterceptor(httpLoggingInterceptor).writeTimeout(30, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+    }
+
+
 
     private static final Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = new Interceptor() {
         @Override
